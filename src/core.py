0138379338d9dee8f4c1051b6713f8229b8845e5
Prismarine Bluefall <sbisonol@gmail.com>
2019-10-30 16:09:23 -0400

    set up db connecting :3

--- src/core.py
+++ src/core.py
@@ -6,7 +6,9 @@ from os import listdir, path
 
 # Third Party Imports
 from discord.ext import commands
-import coloredlogs, asyncio, asyncpg
+import coloredlogs
+import asyncio
+import asyncpg
 
 class Client(commands.Bot):
     def __init__(self, **kwargs) -> None:
@@ -16,13 +18,21 @@ class Client(commands.Bot):
         self.db_address = kwargs["address"] if "address" in kwargs else None
         self.db_username = kwargs["username"] if "username" in kwargs else None
         self.db_password = kwargs["password"] if "password" in kwargs else None
-
         self.db_conn = None
+
+        self.started_up = False
     
     async def on_ready(self):
-        logging.info("%s is online!", self.user.name)
-        logging.info("Connecting to PostgreSQL server...")
-        #TODO Connect to database
+        if not self.started_up:
+            logging.info("%s is online!", self.user.name)
+            logging.info("Connecting to PostgreSQL server...")
+            self.db_conn = await asyncpg.connect(
+                self.db_address, 
+                user=self.db_username, 
+                password=self.db_password
+            )
+            logging.info("Connection established!")
+            self.started_up = True
         
 
     def load_modules(self):
@@ -32,7 +42,7 @@ class Client(commands.Bot):
                 self.load_extension(f"modules.{module[:-3]}")
                 logging.info("Loaded module: '%s'", module)
 
-def main() -> None:
+def main():
     coloredlogs.DEFAULT_FIELD_STYLES.update({'funcName': {'color': 'cyan'}})
     coloredlogs.DEFAULT_FIELD_STYLES["name"]["color"] = 'yellow'
     coloredlogs.DEFAULT_FIELD_STYLES["asctime"]["color"] = 'magenta'
@@ -42,11 +52,11 @@ def main() -> None:
     )
     config = json.load(open("config.json"))
     client = Client(
-        command_prefix=config["prefix"] if "prefix" in config else "rp!", 
-        owner_ids=config["owners"] if "owners" in config else [],
-        address=config["db-address"] if "db-address" in config else None,
-        username=config["db-username"] if "db-username" in config else None,
-        password=config["db-password"] if "db-password" in config else None
+        command_prefix=config["prefix"] if "prefix" in config.keys() else "rp!", 
+        owner_ids=config["owners"] if "owners" in config.keys() else [],
+        address=config["db-address"] if "db-address" in config.keys() else None,
+        username=config["db-username"] if "db-username" in config.keys() else None,
+        password=config["db-password"] if "db-password" in config.keys() else None
         )
     client.load_modules()
     client.run(config["token"])
